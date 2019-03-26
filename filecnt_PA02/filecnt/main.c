e9ceb50f02232273541053c902d9a021f158e425
Aka Dora <akadora0550@gmail.com>
2019-03-25 20:13:23 -0500

    first commit

---
+++ filecnt_PA02/filecnt/main.c
@@ -0,0 +1,77 @@
+//Eunjun Kim
+//exk170230
+//CS3371.0W1
+//03/04/19
+//Nhut Nguyen
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+
+//Function prototype
+void getNumberRecursive(char *directName, int *numBytes, int *numDirect, int *numPlain);
+
+int main(int argc, char **argv) {
+    //Initialize variables
+    int numBytes=0;
+    int numDirectories=0;
+    int numPlainFiles=0;
+
+    //stop if argc is not equal to 2
+    //call the recursive function
+    if (argc != 2) return 0;
+    getNumberRecursive(argv[1], &numBytes, &numDirectories, &numPlainFiles);
+
+    //print out the number of directories, plain files, and bytes occupied in directory
+    printf("The total number of directories in %s is: %i\n", argv[1], numDirectories);
+    printf("The total number of files in %s is: %i\n", argv[1], numPlainFiles);
+    printf("The total number of bytes occupied by all files in %s is: %i\n", argv[1], numBytes);
+    return 0;
+}
+
+//recursively get the number of directories, plain files, and bytes
+void getNumberRecursive(char *directName, int *numBytes, int *numDirect, int *numPlain) {
+    //pointers initialized
+    DIR *dirPtr = opendir(directName);
+    struct dirent *direntPtr;
+
+    //if the pointer points to null, just return
+    if (dirPtr == NULL) {
+        return;
+    }
+
+    //while the pointer is not pointing at null, do the following
+    while ((direntPtr=readdir(dirPtr)) != NULL) {
+        //make a space for the new string
+        char buffer[1000];
+        snprintf(buffer, 1000, "%s/%s", directName, direntPtr->d_name);
+
+        //if the pointer is poiting at the directory, do the following
+        if (direntPtr->d_type == DT_DIR) {
+            //continue as long as the directory name is not . or ..
+            //then increment the number of directory
+            if (!strcmp(direntPtr->d_name, ".") || !strcmp(direntPtr->d_name, "..")) continue;
+            (*numDirect)++;
+
+            //call the recursive function back
+            getNumberRecursive(buffer, numBytes, numDirect, numPlain);
+        }
+        //if the pointer is not pointing at the directory
+        //do the following
+        else if (direntPtr->d_type != DT_DIR) {
+            //increment the number of plain files
+            (*numPlain)++;
+            struct stat fileStat;
+            stat(buffer, &fileStat);
+            //get the total amount of bytes occupied in directory
+            (*numBytes) = *numBytes + fileStat.st_size;
+        }
+    }
+    //close at the end
+    closedir(dirPtr);
+    return;
+}
+
+
---
+++ filedisp_PA02/filedisp/main.c
@@ -0,0 +1,58 @@
+//Eunjun Kim
+//exk170230
+//CS3371.0W1
+//03/04/19
+//Nhut Nguyen
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, char **argv) {
+    //if argc is not 2, then output the
+    //following error message
+    if (argc != 2) {
+        printf("Invalid arguments\n");
+        return -1;
+    }
+    char option = 0;
+
+    //validate the following options:
+    //directories, files, bytes
+    //otherwise print out the error message
+    if(!strcmp(argv[1], "-d")) {
+        option = 'd';
+    } else if (!strcmp(argv[1], "-f")) {
+        option = 'f';
+    } else if (!strcmp(argv[1], "-b")) {
+        option = 'b';
+    } else {
+        printf("Invalid option\n");
+        return -1;
+    }
+
+    //loop through while run is true
+    bool run = true;
+    while (run) {
+        //initialize variables
+        int stringSize = 0;
+        char *line = NULL;
+        //get the line and set equal to size
+        int size = getline(&line, &stringSize, stdin);
+        if (size == -1) break;
+
+        //do the following based on the option
+        if ((option == 'd' && strstr(line, "directories")) ||
+            (option == 'f' && strstr(line, "files")) ||
+            (option == 'b' && strstr(line, "bytes"))) {
+            printf("%s", line);
+            run = false;
+        }
+    }
+
+    return 0;
+}
+
+
+
